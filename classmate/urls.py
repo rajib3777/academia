8701f09c5dd381b071b5885148c8d53d601ec5da
YOUR_NAME <YOUR_EMAIL>
2025-05-19 01:46:51 +0600

    aws s3 bucket add

--- classmate/urls.py
+++ classmate/urls.py
@@ -29,9 +29,4 @@ urlpatterns = [
     path('api/', include("student.urls")),
     # path('sentry-debug/', trigger_error),
     path('chaining/', include('smart_selects.urls')),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
