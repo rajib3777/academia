c529a42e4948bcad679fce49b61ff0859c02ad0b
YOUR_NAME <YOUR_EMAIL>
2025-11-30 21:04:10 +0600

    chore: Add staging and production deployment scripts.

---
+++ .scripts/deploy-staging.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+echo "Deploying the application..."
+
+# Pull the latest changes from the repository
+git pull origin staging
+echo "Pulled latest changes."
+
+# Activate Virtual Environment
+source venv/bin/activate
+echo "Activated virtual environment."
+
+# Install any new dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt --no-input
+
+# Run database migrations
+echo "Running database migrations..."
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Deactivate Virtual Environment
+deactivate
+echo "Deactivated virtual environment."
+
+# Reloading Application so new changes could reflect on website
+pushd classmate
+touch wsgi.py
+popd
+echo "Reloaded application."
+
+# Restart the application services
+sudo systemctl restart gunicorn
+echo "Restarted Gunicorn service."
+sudo systemctl restart nginx
+echo "Restarted Nginx service."
+echo "Deployment completed successfully."
